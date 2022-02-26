88112950d3a839accf59f1ece107a6c1a2e2bc98
Mark O'Connor <mark.oconnor14@gmail.com>
2022-02-26 15:56:53 +0700

    feat: make navbar component

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -15,18 +15,7 @@
         <script src="//unpkg.com/alpinejs" defer></script>
     </head>
     <body>
-        <nav>
-            <a href="/">Home</a>
-            @guest
-                <a href="signup">Sign Up</a>
-                <a href="login">Log In</a>
-            @endguest
-            @auth
-                <a href="record">Record</a>
-                <a href="logout">Log out</a>
-                <p>Welcome back!</p>
-            @endauth
-        </nav>
+        <x-navbar />
         <div>
             <div>
                 <h1>Log In</h1>
