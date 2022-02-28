bd4de7196802c546625790e4583b0ddb740cfa0e
Mark O'Connor <mark.oconnor14@gmail.com>
2022-02-28 17:59:39 +0700

    chore: make header component and style form cols

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,14 +1,12 @@
-<div>
-    <nav>
-        <a href="/">Home</a>
-        @guest
-            <a href="signup">Sign Up</a>
-            <a href="login">Log In</a>
-        @endguest
-        @auth
-            <a href="record">Record</a>
-            <a href="logout">Log out</a>
-            <p>Welcome back!</p>
-        @endauth
-    </nav>
-</div>
+<nav>
+    <a href="/">Home</a>
+    @guest
+        <a href="signup">Sign Up</a>
+        <a href="login">Log In</a>
+    @endguest
+    @auth
+        <a href="record">Record</a>
+        <a href="logout">Log out</a>
+        <p>Welcome back!</p>
+    @endauth
+</nav>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,23 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="app.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <script src="//unpkg.com/alpinejs" defer></script>
-    </head>
-    <body>
+<x-header />
         <x-navbar />
         <div>
-            <div>
+            <div class="col-sm-6 offset-sm-3">
                 <h1>Log In</h1>
                 @if (session()->has('success'))
                     <p
