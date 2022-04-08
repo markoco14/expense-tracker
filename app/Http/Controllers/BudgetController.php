4bc13c5211e316be427112660e1af55944fc7faf
Mark O'Connor <mark.oconnor14@gmail.com>
2022-04-08 23:55:40 +0700

    feat: update budget controller for dynamic user data

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -3,18 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserDeduction;
+use App\Models\UserSalary;
+
 
 class BudgetController extends Controller
 {
     public function index() {
-        $monthlySalary = 53000;
-        $labourInsurance = 962;
-        $nationalHealthInsurance = 822;
+
+        $monthlySalary = UserSalary::where('user_id', auth()->user()->id)
+            ->pluck('salary_amount')
+            ->toArray()[0];
+        // dd($monthlySalary);
+        $labourInsurance = UserDeduction::where('user_id', auth()->user()->id)
+            ->where('deduction_name', 'li')
+            ->pluck('deduction_amount')
+            ->toArray()[0];
+        $nationalHealthInsurance = UserDeduction::where('user_id', auth()->user()->id)
+            ->where('deduction_name', 'nhi')
+            ->pluck('deduction_amount')
+            ->toArray()[0];
         $deductions = $labourInsurance + $nationalHealthInsurance;
         $takeHomePay = $monthlySalary - $deductions;
-        $rent = 16800;
-        $utilities = 1035;
-        $savings = 15000;
+        $rent = UserDeduction::where('user_id', auth()->user()->id)
+            ->where('deduction_name', 'rent')
+            ->pluck('deduction_amount')
+            ->toArray()[0];
+        $utilities = UserDeduction::where('user_id', auth()->user()->id)
+            ->where('deduction_name', 'utilities')
+            ->pluck('deduction_amount')
+            ->toArray()[0];
+        $savings = UserDeduction::where('user_id', auth()->user()->id)
+            ->where('deduction_name', 'savings')
+            ->pluck('deduction_amount')
+            ->toArray()[0];
         $beforeDailyExpenses = $takeHomePay - $rent - $utilities - $savings;
         $totalDailyBudget = 34 * 500;
         $surplus = $beforeDailyExpenses - $totalDailyBudget;
