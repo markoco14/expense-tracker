28049daa20ea820476477e16edd6d8768fd97d4b
Mark O'Connor <mark.oconnor14@gmail.com>
2022-04-08 23:25:01 +0700

    feat: create user_salaries migration and model

---
+++ database/migrations/2022_04_08_155315_create_user_salaries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserSalariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_salaries', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('salary_amount');
+            $table->string('job_category');
+            $table->string('job_title');
+            $table->string('salary_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_salaries');
+    }
+}
