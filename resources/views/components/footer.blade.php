8cd55c2858744377dbde781ec887bfeb86e55a29
Mark O'Connor <mark.oconnor14@gmail.com>
2022-05-29 20:50:02 +0700

    feat: add hamburger nav script to footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,19 @@
-    <footer class="">
+    <footer>
         <p>all rights reserved c 2022</p>
-        <p class="">thank you for playing</p>
+        <p>thank you for playing</p>
     </footer>
     {{-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script> --}}
     {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script> --}}
+    <script async defer>
+        const hamburgerMenu = document.getElementById('hamburger-menu');
+        const hamburgerToggle = document.getElementById('hamburger-toggle');
+        hamburgerToggle.addEventListener('click', ()=>{
+            if(hamburgerMenu.getAttribute('class') === 'hamburger-block') {
+                hamburgerMenu.setAttribute('class', 'hamburger-block-hidden');
+            } else {
+                hamburgerMenu.setAttribute('class', 'hamburger-block');
+            }
+        });
+    </script>
     </body>
 </html>
