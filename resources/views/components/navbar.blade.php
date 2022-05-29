8cc9703dfcc08348b76d5efd3e8785bfe6d2d8d7
Mark O'Connor <mark.oconnor14@gmail.com>
2022-05-29 20:49:45 +0700

    feat: rework navbar for hamburger menu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,29 +1,31 @@
+<nav class="nav">
+    @guest
+        <p><strong>Expensave</strong></p>
+        <div>
+            <a href="login">Log In</a>
+            <i id="hamburger-toggle" class="fas fa-bars"></i>
+        </div>
+    @endguest
+    @auth
+        <p><strong>Expensave</strong></p>
+        <div>
+            <a href="tracking">Tracking</a>
+            <a href="progress">Progress</a>
+            <i id="hamburger-toggle" class="fas fa-bars"></i>
+        </div>
+    @endauth
+</nav>
 @guest
-    <nav class="nav">
+    <div id="hamburger-menu" class="hamburger-block-hidden">
+        <a href="/">Home</a>
+        <a href="signup">Sign Up</a>
+    </div>            
 @endguest
 @auth
-    <nav class="nav">
+    <div id="hamburger-menu" class="hamburger-block-hidden">
+        <a href="spending">Spending</a>
+        <a href="profile">Profile</a>
+        <a href="details">Details</a>
+        <a href="logout">Log out</a>
+    </div>
 @endauth
-        @guest
-            <p><strong>Expensave</strong></p>            
-        @endguest
-        @auth
-            <p><strong>Expensave</strong></p>
-        @endauth
-        
-        <div>
-            @guest
-                <a href="/">Home</a>
-                <a href="signup">Sign Up</a>
-                <a href="login">Log In</a>
-            @endguest
-            @auth
-                <a href="tracking">Tracking</a>
-                <a href="spending">Spending</a>
-                <a href="profile">Profile</a>
-                <a href="details">Details</a>
-                <a href="progress">Progress</a>
-                <a href="logout">Log out</a>
-            @endauth
-        </div>
-</nav>
