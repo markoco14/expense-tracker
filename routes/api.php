5913934f0c332b3fc532b9ec0fa1e1b070da91f5
Mark O'Connor <mark.oconnor14@gmail.com>
2022-05-30 22:52:11 +0700

    feat: create progress page total spending api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BudgetController;
+use App\Models\Expense;
+use App\Models\UserBudget;
+
+// Auth
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,37 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('progress', [BudgetController::class, 'percent']);
+
+// Route::get('progress', function() {
+            
+    // dd(auth()->user());
+    // $expenses = Expense::where('username', auth()->user()->username)
+    // ->get()
+    // ->toArray();
+
+    // return $expenses;
+    // $totalSpent = 0;
+
+    // foreach($expenses as &$expense) {
+    //     $expense['created_at'] = Carbon\Carbon::parse($expense['created_at'])->toDateString();
+    //     if ($expense['created_at'] === Carbon\Carbon::today()->toDateString()) {
+    //         array_push($expensesToday, $expense['amount']);
+    //         $totalSpent += $expense['amount'];
+    //     }
+    // }
+
+    // $budget = UserBudget::where('user_id', auth()->user()->id)
+    //     ->where('budget_status', 'CURRENT')
+    //     ->get()
+    //     ->toArray()[0]['budget_amount'];
+
+    // $percentSpent = $totalSpent/$budget*100;
+    // return $percentSpent;
+    // return response()->json(['data' => $percentSpent], 200);
+    // return response()->json([
+    //     "response" => "you returned this",
+    // ], 200);
+//     return json_encode("You returned this.");
+// });
