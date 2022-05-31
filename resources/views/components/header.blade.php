e6c4286db3b9b618bed45df783723735751e7236
Mark O'Connor <mark.oconnor14@gmail.com>
2022-05-31 22:09:32 +0700

    chore: fix styles for heroku

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -11,8 +11,8 @@
 
         <!-- Styles -->
         {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> --}}
-        <link rel="stylesheet" href="{{URL::asset("/css/styles.css")}}">
-        {{-- <link rel="stylesheet" href="{{URL::asset("/css/styles.css", 'secure')}}"> --}}
+        {{-- <link rel="stylesheet" href="{{URL::asset("/css/styles.css")}}"> --}}
+        <link rel="stylesheet" href="{{URL::asset("/css/styles.css", 'secure')}}">
         {{-- <link rel="stylesheet" href="{{URL::asset("/css/styles.css", 'secure')}}"> --}}
         <script async src="https://kit.fontawesome.com/d0c81e3c08.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/chart.js@3.8.0/dist/chart.min.js"></script>
