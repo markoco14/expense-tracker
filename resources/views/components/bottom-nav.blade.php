34dc921fcc5c0bc31dee332d052a92b81c984649
Mark O'Connor <mark.oconnor14@gmail.com>
2022-06-03 16:21:36 +0700

    chore: separate primary and bottom nav components

---
+++ resources/views/components/bottom-nav.blade.php
@@ -0,0 +1,23 @@
+<nav>
+    <ul>
+    @guest
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="login">Log In</a>
+        </li>
+    @endguest
+    @auth
+        <li>
+            <a href="tracking">Tracking</a>
+        </li>
+        <li>
+            <a href="progress">Progress</a>
+        </li>            
+    @endauth
+        <button class="primary-nav-toggle">
+            <i id="hamburger-toggle" class="fas fa-2x fa-bars"></i>
+        </button class="primary-nav-toggle">
+    </ul>
+</nav>
---
+++ resources/views/components/primary-nav.blade.php
@@ -0,0 +1,32 @@
+<header class="primary-header flex">
+    <div class='logo'>
+        <p>Expensave</p>
+    </div>
+    <ul id="hamburger-menu" class="secondary-navigation flex hamburger-block-hidden">
+    @guest
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="signup">Sign Up</a>
+        </li>
+        <li>
+            <a href="login">Log In</a>
+        </li>
+        @endguest
+        @auth
+            <li>
+                <a href="spending">Spending</a>
+            </li>
+            <li>
+                <a href="profile">Profile</a>
+            </li>
+            <li>
+                <a href="details">Details</a>
+            </li>
+            <li>
+                <a href="logout">Log out</a>
+            </li>
+        @endauth
+    </ul>
+</header>
