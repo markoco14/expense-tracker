dc41583999f5328c4d2135b8bfc62f1523f2b5b4
Mark O'Connor <mark.oconnor14@gmail.com>
2022-06-10 00:28:50 +0700

    feat: set font family for site

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -7,7 +7,11 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{URL::asset("/css/styles.css")}}">
@@ -17,3 +21,4 @@
     </head>
     <body>
         <x-primary-nav />
+        <main>
