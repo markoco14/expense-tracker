fcd2732afecfc13794c39e76ea2fe7c94204e5db
Mark O'Connor <mark.oconnor14@gmail.com>
2022-06-16 23:07:27 +0700

    feat: make react salary component

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -23,13 +23,24 @@ class SalaryController extends Controller
         return redirect('profile');
     }
 
-    public function getUpdatedSalary($userid, $name) {
-        // return ['response' => "You are getting the update Salary now"];
+    public function getAll($userid) {
         $salary = UserSalary::where('user_id', $userid)
-        // ->where('salary_name', $name)
         ->where('salary_status', 'CURRENT')
         ->get();
         return json_encode($salary);
+    }
+
+    public function create($userid, $amount) {
+        // return ['response' => 'You landed in the create function of the salary controller'];
+        $salary = new UserSalary;
+        $salary->user_id = $userid;
+        $salary->salary_amount = $amount;
+        $salary->salary_status = 'CURRENT';
+        $salary->job_category = 'NULL';
+        $salary->job_title = 'NULL';
+        $salary->month = Carbon::now()->month;
+        $salary->save();
+        return ['status' => 201];
     }
 
     public function update($userid, $amount) {
@@ -41,6 +52,16 @@ class SalaryController extends Controller
             'amount' => $amount,
         ];
     }
+
+    public function getUpdatedSalary($userid, $name) {
+        // return ['response' => "You are getting the update Salary now"];
+        $salary = UserSalary::where('user_id', $userid)
+        // ->where('salary_name', $name)
+        ->where('salary_status', 'CURRENT')
+        ->get();
+        return json_encode($salary);
+    }
+
     // update with salary param later
     public function delete($userid) {
         UserSalary::where('user_id', $userid)
--- routes/api.php
+++ routes/api.php
@@ -25,21 +25,26 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // PROFILE PAGE API ROUTES
-// GET ROUTES
+// READ ROUTES
+Route::get('profile/salary/{userid}', [SalaryController::class, 'getAll']);
+
+// READ UPDATED ROUTES
 Route::get('profile/salary/get/{userid}/{salary}', [SalaryController::class, 'getUpdatedSalary']);
 Route::get('profile/deduction/get/{userid}/{deduction}', [DeductionController::class, 'getUpdatedDeduction']);
 Route::get('profile/saving/get/{userid}', [SavingController::class, 'getUpdatedSaving']);
 Route::get('profile/budget/get/{userid}', [BudgetController::class, 'getUpdatedBudget']);
+// CREATE ROUTES
+Route::post('profile/salary/create/{userid}/{salary}', [SalaryController::class, 'create']);
 
-// DELETE ROUTES
-Route::post('profile/salary/delete/{userid}', [SalaryController::class, 'delete']);
-Route::post('profile/deduction/delete/{userid}/{deduction}', [DeductionController::class, 'delete']);
-Route::post('profile/saving/delete/{userid}', [SavingController::class, 'delete']);
-Route::post('profile/budget/delete/{userid}', [BudgetController::class, 'delete']);
 // EDIT ROUTES
 Route::post('profile/salary/edit/{userid}/{salary_amount}', [SalaryController::class, 'update']);
 Route::post('profile/deduction/edit/{userid}/{deduction}/{amount}/{original_name}', [DeductionController::class, 'update']);
 Route::post('profile/saving/edit/{userid}/{saving_amount}', [SavingController::class, 'update']);
 Route::post('profile/budget/edit/{userid}/{budget_amount}', [BudgetController::class, 'update']);
+// DELETE ROUTES
+Route::post('profile/salary/delete/{userid}', [SalaryController::class, 'delete']);
+Route::post('profile/deduction/delete/{userid}/{deduction}', [DeductionController::class, 'delete']);
+Route::post('profile/saving/delete/{userid}', [SavingController::class, 'delete']);
+Route::post('profile/budget/delete/{userid}', [BudgetController::class, 'delete']);
 // PROGRESS PAGE API ROUTES
 Route::get('percent/{username}', [BudgetController::class, 'getTodaySpendingPercent']);
